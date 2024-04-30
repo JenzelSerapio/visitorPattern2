5556343fd0507c6d36b7a9f75402b08fb18d8478
Jenzel Serapio <142371031+JenzelSerapio@users.noreply.github.com>
2024-04-30 15:36:13 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,19 +3,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        // Create some furniture items
+
         List<Furniture> furnitureList = new ArrayList<>();
         furnitureList.add(new Chair(5));
         furnitureList.add(new Table(2, 3));
         furnitureList.add(new Sofa(2, 2));
 
-        // Calculate shipping cost
         ShippingCostCalculator calculator = new ShippingCostCalculator();
         for (Furniture furniture : furnitureList) {
             furniture.accept(calculator);
         }
 
-        // Output total shipping cost
+
         System.out.println("----------------------------------------------------");
         System.out.println("Total shipping cost for all items: $" + calculator.getTotalCost());
     }
